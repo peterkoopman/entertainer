9925566f611baf7512d245d193e95fb058f40bd2
Peter Koopman <peter@scribbledesign.co.nz>
2025-06-13 16:01:04 +1200

    Switch icons to Material Symbols

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,12 +24,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/icon?family=Material+Icons"
-        />
-      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} sidebar-layout`}>
         {children}
