7f06943b2d828ef02b410d2343c0738fa823cba0
Peter Koopman <peter@scribbledesign.co.nz>
2025-06-24 16:34:59 +1200

    Add locale to datepicker

--- src/components/BookingSearch/BookingSearch.tsx
+++ src/components/BookingSearch/BookingSearch.tsx
@@ -1,11 +1,12 @@
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import style from './BookingSearch.module.css';
+import 'dayjs/locale/en-gb';
 
 const BookingSearch = () => {
   return (
     <div className={style.search}>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="en-gb">
         <DatePicker label="Search bookings" sx={{ width: '100%' }} />
       </LocalizationProvider>
     </div>
