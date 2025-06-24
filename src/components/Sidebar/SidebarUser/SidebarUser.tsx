fa87f0b87afbc18c0f5fc76a407e103d14c1897b
Peter Koopman <peter@scribbledesign.co.nz>
2025-06-24 14:14:10 +1200

    Get user details for sidebar user

--- src/components/Sidebar/SidebarUser/SidebarUser.tsx
+++ src/components/Sidebar/SidebarUser/SidebarUser.tsx
@@ -2,11 +2,17 @@
 
 import { useState } from 'react';
 import { Avatar } from '@mui/material';
+import { useUser } from '@/hooks/useUser';
 import SidebarUserPopup from '../SidebarUserPopup/SidebarUserPopup';
 import style from './SidebarUser.module.css';
 
 const SidebarUser = ({ sidebarOpen }: { sidebarOpen: boolean }) => {
   const [showPopup, setShowPopup] = useState(false);
+  const { user, profile, loading } = useUser();
+
+  if (loading && !user) {
+    return <h1>Loading...</h1>;
+  }
 
   const togglePopup = () => {
     setShowPopup(!showPopup);
@@ -19,11 +25,11 @@ const SidebarUser = ({ sidebarOpen }: { sidebarOpen: boolean }) => {
         <button
           className={`${style.userDisplay} ${sidebarOpen ? '' : style.closed}`}
           onClick={togglePopup}>
-          <Avatar src={'/PK_profile_pic.jpg'} alt={'Peter Koopman'} />
+          <Avatar src={profile?.avatar_url} alt={profile?.full_name} />
           <div
             className={`${style.userText} ${sidebarOpen ? '' : style.closed}`}>
-            <p className={style.userName}>Peter Koopman</p>
-            <p className={style.userEmail}>peter@scribbledesign.co.nz</p>
+            <p className={style.userName}>{profile?.full_name}</p>
+            <p className={style.userEmail}>{user?.email}</p>
           </div>
         </button>
       </div>
