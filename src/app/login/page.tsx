54731ccf046c29ef079c73c3b3015697cf5627b8
Peter Koopman <peter@scribbledesign.co.nz>
2025-06-24 14:14:45 +1200

    Add logo to login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,12 +3,17 @@
 import { login, signup } from './actions';
 import { Button, TextField, ThemeProvider } from '@mui/material';
 import { theme } from '@/utils/muiThemes';
+import Logo from '@/components/Logo/Logo';
+import style from './login.module.css';
 
 export default function LoginPage() {
   return (
     <main>
       <ThemeProvider theme={theme}>
         <div className="utility-page">
+          <div className={style.logo}>
+            <Logo closed={false} />
+          </div>
           <h2>Log in or Sign up</h2>
           <form className="login-form">
             <TextField name="email" type="email" required label="Email">
