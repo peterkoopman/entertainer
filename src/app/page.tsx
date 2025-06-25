84bf27cb27654615c37fecf9d6a08b198e9dc91e
Peter Koopman <peter@scribbledesign.co.nz>
2025-06-25 14:29:46 +1200

    Remove unnecessary JSX code from homepage - just redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
 'use client';
 
-import SidebarState from '@/context/SidebarContext';
 import { redirect } from 'next/navigation';
 
 export default function Home() {
-  // TODO: determine whether user is logged in. Redirect to dsahboard or login page
-  return (
-    <SidebarState>
-      <div>{redirect('/dashboard')}</div>
-    </SidebarState>
-  );
+  redirect('/dashboard');
 }
