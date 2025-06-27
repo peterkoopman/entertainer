b14a1456c239451afa9e262965c3402888116ae0
Peter Koopman <peter@scribbledesign.co.nz>
2025-06-27 15:49:01 +1200

    Replace useSkills hook with server actions

--- src/app/(appPages)/account/actions.ts
+++ src/app/(appPages)/account/actions.ts
@@ -1,3 +1,46 @@
 'use server';
 
-// import { createClient } from '@/utils/supabase/server';
+import { createClient } from '@/utils/supabase/server';
+
+export interface Skillset {
+  id: string;
+  name: string;
+}
+
+export async function fetchSkills(
+  user_id: string | undefined
+): Promise<Skillset[] | null> {
+  const supabase = createClient();
+  if (!user_id) {
+    return null;
+  }
+
+  const { data, error } = await (await supabase)
+    .from('user_skillset')
+    .select(`skill(id, name)`)
+    .eq('user_id', user_id);
+
+  if (error) {
+    console.error('Error fetching skills:', error.message);
+    return null;
+  }
+
+  const skills: Skillset[] = data.map((item) => item.skill as Skillset);
+  console.log(skills);
+  return skills;
+}
+
+export async function fetchAllSkills(): Promise<Skillset[] | null> {
+  const supabase = createClient();
+
+  const { data, error } = await (await supabase)
+    .from('skill')
+    .select(`id, name`);
+
+  if (error) {
+    console.error('Error fetching all skills:', error.message);
+    return null;
+  }
+
+  return data;
+}
