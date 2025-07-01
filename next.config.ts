b13e81c7188af1cf9b3cf00d8c229364c6fe4486
Peter Koopman <peter@scribbledesign.co.nz>
2025-07-01 15:48:01 +1200

    Increase form size limit to accommodate avatar file

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false, // Disables next dev tools in the browser
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
+  },
 };
 
 export default nextConfig;
