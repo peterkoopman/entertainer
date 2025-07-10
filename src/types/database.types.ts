c36ee3004e5be1733d6577b87f391aeb1b24f65e
Peter Koopman <peter@scribbledesign.co.nz>
2025-07-10 14:20:15 +1200

    Update database types

--- src/types/database.types.ts
+++ src/types/database.types.ts
@@ -7,6 +7,11 @@ export type Json =
   | Json[];
 
 export type Database = {
+  // Allows to automatically instanciate createClient with right options
+  // instead of createClient<Database, { PostgrestVersion: 'XX' }>(URL, KEY)
+  __InternalSupabase: {
+    PostgrestVersion: '12.2.3 (519615d)';
+  };
   public: {
     Tables: {
       booking: {
@@ -22,8 +27,12 @@ export type Database = {
           job_notes: string | null;
           load_in: string | null;
           personnel_notes: string | null;
+          setup_id: number | null;
           soundcheck: string | null;
           start_time: string | null;
+          status_id: number | null;
+          tax_type_id: number | null;
+          type_id: number | null;
           venue_name: string | null;
         };
         Insert: {
@@ -38,8 +47,12 @@ export type Database = {
           job_notes?: string | null;
           load_in?: string | null;
           personnel_notes?: string | null;
+          setup_id?: number | null;
           soundcheck?: string | null;
           start_time?: string | null;
+          status_id?: number | null;
+          tax_type_id?: number | null;
+          type_id?: number | null;
           venue_name?: string | null;
         };
         Update: {
@@ -54,8 +67,12 @@ export type Database = {
           job_notes?: string | null;
           load_in?: string | null;
           personnel_notes?: string | null;
+          setup_id?: number | null;
           soundcheck?: string | null;
           start_time?: string | null;
+          status_id?: number | null;
+          tax_type_id?: number | null;
+          type_id?: number | null;
           venue_name?: string | null;
         };
         Relationships: [
@@ -65,8 +82,99 @@ export type Database = {
             isOneToOne: false;
             referencedRelation: 'client';
             referencedColumns: ['id'];
+          },
+          {
+            foreignKeyName: 'booking_setup_id_fkey';
+            columns: ['setup_id'];
+            isOneToOne: false;
+            referencedRelation: 'booking_setup';
+            referencedColumns: ['id'];
+          },
+          {
+            foreignKeyName: 'booking_status_id_fkey';
+            columns: ['status_id'];
+            isOneToOne: false;
+            referencedRelation: 'booking_status';
+            referencedColumns: ['id'];
+          },
+          {
+            foreignKeyName: 'booking_tax_type_id_fkey';
+            columns: ['tax_type_id'];
+            isOneToOne: false;
+            referencedRelation: 'tax_type';
+            referencedColumns: ['id'];
+          },
+          {
+            foreignKeyName: 'booking_type_id_fkey';
+            columns: ['type_id'];
+            isOneToOne: false;
+            referencedRelation: 'booking_type';
+            referencedColumns: ['id'];
           }
         ];
+      };
+      booking_setup: {
+        Row: {
+          created_at: string;
+          description: string | null;
+          id: number;
+          name: string | null;
+        };
+        Insert: {
+          created_at?: string;
+          description?: string | null;
+          id?: number;
+          name?: string | null;
+        };
+        Update: {
+          created_at?: string;
+          description?: string | null;
+          id?: number;
+          name?: string | null;
+        };
+        Relationships: [];
+      };
+      booking_status: {
+        Row: {
+          created_at: string;
+          description: string | null;
+          id: number;
+          name: string | null;
+        };
+        Insert: {
+          created_at?: string;
+          description?: string | null;
+          id?: number;
+          name?: string | null;
+        };
+        Update: {
+          created_at?: string;
+          description?: string | null;
+          id?: number;
+          name?: string | null;
+        };
+        Relationships: [];
+      };
+      booking_type: {
+        Row: {
+          created_at: string;
+          description: string | null;
+          id: number;
+          name: string | null;
+        };
+        Insert: {
+          created_at?: string;
+          description?: string | null;
+          id?: number;
+          name?: string | null;
+        };
+        Update: {
+          created_at?: string;
+          description?: string | null;
+          id?: number;
+          name?: string | null;
+        };
+        Relationships: [];
       };
       client: {
         Row: {
@@ -78,6 +186,7 @@ export type Database = {
           email: string | null;
           id: number;
           name: string | null;
+          notes: string | null;
           phone: string | null;
           updated_at: string | null;
         };
@@ -90,6 +199,7 @@ export type Database = {
           email?: string | null;
           id?: number;
           name?: string | null;
+          notes?: string | null;
           phone?: string | null;
           updated_at?: string | null;
         };
@@ -102,6 +212,7 @@ export type Database = {
           email?: string | null;
           id?: number;
           name?: string | null;
+          notes?: string | null;
           phone?: string | null;
           updated_at?: string | null;
         };
@@ -138,6 +249,27 @@ export type Database = {
         };
         Update: {
           created_at?: string;
+          id?: number;
+          name?: string | null;
+        };
+        Relationships: [];
+      };
+      tax_type: {
+        Row: {
+          created_at: string;
+          description: string | null;
+          id: number;
+          name: string | null;
+        };
+        Insert: {
+          created_at?: string;
+          description?: string | null;
+          id?: number;
+          name?: string | null;
+        };
+        Update: {
+          created_at?: string;
+          description?: string | null;
           id?: number;
           name?: string | null;
         };
@@ -276,21 +408,28 @@ export type Database = {
   };
 };
 
-type DefaultSchema = Database[Extract<keyof Database, 'public'>];
+type DatabaseWithoutInternals = Omit<Database, '__InternalSupabase'>;
+
+type DefaultSchema = DatabaseWithoutInternals[Extract<
+  keyof Database,
+  'public'
+>];
 
 export type Tables<
   DefaultSchemaTableNameOrOptions extends
     | keyof (DefaultSchema['Tables'] & DefaultSchema['Views'])
-    | { schema: keyof Database },
+    | { schema: keyof DatabaseWithoutInternals },
   TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof Database;
+    schema: keyof DatabaseWithoutInternals;
   }
-    ? keyof (Database[DefaultSchemaTableNameOrOptions['schema']]['Tables'] &
-        Database[DefaultSchemaTableNameOrOptions['schema']]['Views'])
+    ? keyof (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions['schema']]['Tables'] &
+        DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions['schema']]['Views'])
     : never = never
-> = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
-  ? (Database[DefaultSchemaTableNameOrOptions['schema']]['Tables'] &
-      Database[DefaultSchemaTableNameOrOptions['schema']]['Views'])[TableName] extends {
+> = DefaultSchemaTableNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals;
+}
+  ? (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions['schema']]['Tables'] &
+      DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions['schema']]['Views'])[TableName] extends {
       Row: infer R;
     }
     ? R
@@ -308,14 +447,16 @@ export type Tables<
 export type TablesInsert<
   DefaultSchemaTableNameOrOptions extends
     | keyof DefaultSchema['Tables']
-    | { schema: keyof Database },
+    | { schema: keyof DatabaseWithoutInternals },
   TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof Database;
+    schema: keyof DatabaseWithoutInternals;
   }
-    ? keyof Database[DefaultSchemaTableNameOrOptions['schema']]['Tables']
+    ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions['schema']]['Tables']
     : never = never
-> = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
-  ? Database[DefaultSchemaTableNameOrOptions['schema']]['Tables'][TableName] extends {
+> = DefaultSchemaTableNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals;
+}
+  ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions['schema']]['Tables'][TableName] extends {
       Insert: infer I;
     }
     ? I
@@ -331,14 +472,16 @@ export type TablesInsert<
 export type TablesUpdate<
   DefaultSchemaTableNameOrOptions extends
     | keyof DefaultSchema['Tables']
-    | { schema: keyof Database },
+    | { schema: keyof DatabaseWithoutInternals },
   TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof Database;
+    schema: keyof DatabaseWithoutInternals;
   }
-    ? keyof Database[DefaultSchemaTableNameOrOptions['schema']]['Tables']
+    ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions['schema']]['Tables']
     : never = never
-> = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
-  ? Database[DefaultSchemaTableNameOrOptions['schema']]['Tables'][TableName] extends {
+> = DefaultSchemaTableNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals;
+}
+  ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions['schema']]['Tables'][TableName] extends {
       Update: infer U;
     }
     ? U
@@ -354,14 +497,16 @@ export type TablesUpdate<
 export type Enums<
   DefaultSchemaEnumNameOrOptions extends
     | keyof DefaultSchema['Enums']
-    | { schema: keyof Database },
+    | { schema: keyof DatabaseWithoutInternals },
   EnumName extends DefaultSchemaEnumNameOrOptions extends {
-    schema: keyof Database;
+    schema: keyof DatabaseWithoutInternals;
   }
-    ? keyof Database[DefaultSchemaEnumNameOrOptions['schema']]['Enums']
+    ? keyof DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions['schema']]['Enums']
     : never = never
-> = DefaultSchemaEnumNameOrOptions extends { schema: keyof Database }
-  ? Database[DefaultSchemaEnumNameOrOptions['schema']]['Enums'][EnumName]
+> = DefaultSchemaEnumNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals;
+}
+  ? DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions['schema']]['Enums'][EnumName]
   : DefaultSchemaEnumNameOrOptions extends keyof DefaultSchema['Enums']
   ? DefaultSchema['Enums'][DefaultSchemaEnumNameOrOptions]
   : never;
@@ -369,14 +514,16 @@ export type Enums<
 export type CompositeTypes<
   PublicCompositeTypeNameOrOptions extends
     | keyof DefaultSchema['CompositeTypes']
-    | { schema: keyof Database },
+    | { schema: keyof DatabaseWithoutInternals },
   CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
-    schema: keyof Database;
+    schema: keyof DatabaseWithoutInternals;
   }
-    ? keyof Database[PublicCompositeTypeNameOrOptions['schema']]['CompositeTypes']
+    ? keyof DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions['schema']]['CompositeTypes']
     : never = never
-> = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicCompositeTypeNameOrOptions['schema']]['CompositeTypes'][CompositeTypeName]
+> = PublicCompositeTypeNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals;
+}
+  ? DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions['schema']]['CompositeTypes'][CompositeTypeName]
   : PublicCompositeTypeNameOrOptions extends keyof DefaultSchema['CompositeTypes']
   ? DefaultSchema['CompositeTypes'][PublicCompositeTypeNameOrOptions]
   : never;
